07a17ea394b02ac7a28b8c6510e90c9fc9c4e65e
ndkhoi192 <ndkhoi192@gmail.com>
2026-03-16 16:19:12 +0700

    update url for deploy

--- front-end/src/services/socket.js
+++ front-end/src/services/socket.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
-const SOCKET_URL = 'http://localhost:3000'; // Đổi theo config trong file .env hoặc env variables
+const SOCKET_URL = import.meta.env.VITE_SOCKET_URL || 'http://localhost:3000'; // Đổi theo config trong file .env hoặc env variables
 
 export const socket = io(SOCKET_URL, {
     autoConnect: false, // Để khi nào cần mới chủ động connect
